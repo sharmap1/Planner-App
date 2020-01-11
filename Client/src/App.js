f3ece6a1ad56146572a0c7813f1728a448664fae
Prasamsha Sharma <prasamshari@gmail.com>
2020-01-10 16:02:10 -0800

    added joke api and formatted the app

--- Client/src/App.js
+++ Client/src/App.js
@@ -6,6 +6,8 @@ import Alert from "./components/Alert";
 import uuid from "uuid/v4";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Nav from "./components/Nav";
+import DayJoke from "./components/DayJoke";
+import DayQuote from "./components/DayQuote";
 import { Col, Row, Container, Card } from "react-bootstrap";
 
 const initialTasks = {
@@ -113,12 +115,11 @@ function App() {
       <main className="App">
         <Container>
           <Row>
-            {/* <Col xs={8}>
-             */}
-            <Col md={{ span: 8, offset: 2 }}>
+            <Col md={{ span: 12, offset: 0 }}>
+              <Nav />
+            </Col>
+            <Col md={{ span: 6, offset: 0 }}>
               <Card.Body className="sub-card">
-                <Nav />
-                <Card.Body> </Card.Body>
                 <Card.Header as="h5">My TODO</Card.Header>
                 <Card.Body>
                   <TaskForm
@@ -140,6 +141,19 @@ function App() {
                   />
                 </Card.Body>
                 <Card.Footer className="quote">Work hard dream Big</Card.Footer>
+              </Card.Body>
+            </Col>
+
+            <Col md={{ span: 3, offset: 0 }}>
+              <Card.Body className="joke-card">
+                <Card.Header as="h5">My Jokes</Card.Header>
+                <DayJoke />
+              </Card.Body>
+            </Col>
+            <Col md={{ span: 3, offset: 0 }}>
+              <Card.Body className="joke-card">
+                <Card.Header as="h5">My Quotes</Card.Header>
+                <DayQuote />
               </Card.Body>
             </Col>
           </Row>
---
+++ Client/src/components/DayQuote/index.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const DayJoke = () => {
+  const [quote, setJoke] = useState("");
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        "http://api.paperquotes.com/apiv1/quotes/?limit=1&offset=1&order=-likes&order=%3F&tags=flower%2Csun"
+      );
+      // console.log(result.data);
+      setJoke(`${result.data[0].quote} ${result.data[0].author}`);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <div className="joke-card">
+        <p>{quote}</p>
+        <p>“Be yourself; everyone else is already taken.” ― Oscar Wilde</p>
+      </div>
+    </>
+  );
+};
+export default DayJoke;
