b22c04582c2cd80a1d4552476ff71f164c3ff093
Frederica Blissett aka Samri <redricasa@users.noreply.github.com>
2020-01-15 10:08:21 -0800

    - npm installed  react-beautiful-dnd - added more to server.js to notify on console about mongodb connection - added rputes connection to server for local server connection

--- Routes/routes.js
+++ Routes/routes.js
@@ -1,5 +1,6 @@
-const express = require('express');
-const router = express.Router();
+const router = require('express').Router();
+let Task = require('../Models/todo');
+// TODO: 
 
 // my TODO routes
     // task add
--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const PORT = process.env.PORT || 4000;
 const cors = require('cors');
 const app = express();
 const mongoose = require('mongoose');
-//const routes = require('./routes/routes.index');
-
 // Define middleware here
 app.use(cors())
 app.use(express.urlencoded({ extended: true }));
@@ -13,15 +11,19 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 };
-// Define API routes here
-//app.use(routes);
 // connect to Mongo DB
 mongoose.connect(
   // TODO- set up mlab on heroku and replace credentials below
   process.env.MONGODB_URI || "mongodb://user:password1@ds035310.mlab.com:35310/heroku_mmkdvd75",
   { useNewUrlParser: true }
-
 );
+// to let us know database connection established
+connection.once('open', () => {
+  console.log("----MongoDB database connection established successfully!----")
+});
+// connect and use router
+const routes = require('./Routes/routes');
+app.use('/', routes);
 //PORT listening on 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
