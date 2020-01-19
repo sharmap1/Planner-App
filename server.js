7bb961004f106d5c7c0eeaf8bd750ec6e5849a27
Prasamsha Sharma <prasamshari@gmail.com>
2020-01-19 10:47:29 -0800

    changed the client to uppercase C

--- server.js
+++ server.js
@@ -10,7 +10,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("Client/build"));
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./Client/build/index.html"));
+  });
 }
 // connect to Mongo DB
 mongoose.connect(
@@ -27,9 +30,7 @@ mongoose.connect(
 const routes = require("./Routes/routes");
 app.use("/", routes);
 // app.use("/api", routes);
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./Client/build/index.html"));
-});
+
 //PORT listening on
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
